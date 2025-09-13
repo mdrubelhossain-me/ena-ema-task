0c628eb9c67490fbb8bd8f64e93b8d37a5c46537
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-09-13 16:59:57 +0600

    hero complete

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import Image from "next/image";
+import logo from "../../../../public/assets/images/Logo.svg";
+import Sparkle from "../../../../public/assets/images/Sparkle.svg";
 
 const Hero: React.FC = () => {
   return (
-    <section>
-      <h1>Welcome to the Hero Section</h1>
-      <p className={styles.p}>This is a placeholder for your hero content.</p>
+    <section className={styles.heroContainer}>
+      <div className={styles.logoContainer}>
+        <Image src={logo} alt="Logo" className={styles.logo} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.trustedBy}>
+          <Image
+            src={Sparkle}
+            width={16}
+            height={16}
+            alt="Sparkle"
+            className={styles.sparkle}
+          />
+          <span>Trusted by 30+ startups & entrepreneurs worldwide</span>
+        </div>
+        <h1 className={styles.title}>
+          Strategic Landing Pages That Convert From Just <span>$200</span>
+        </h1>
+        <h5 className={styles.subtitle}>
+          Stop wasting traffic on generic pages. We deliver premium,
+          conversion-focused landing pages designed, developed, and launched in
+          just 5 days.
+        </h5>
+      </div>
     </section>
   );
 };
